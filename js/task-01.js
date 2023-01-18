29225ff331252e846728e13c1c0834dedef02ae9
Olenteva74 <olenteva74@ukr.net>
2023-01-18 16:24:10 +0200

    firstElementChild and lastElementChild

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const listOfClassItem = listOfCategory.querySelectorAll(".item");
 console.log(`Number of categories: ${listOfClassItem.length}`);
 
 listOfClassItem.forEach((item) => {
-    console.log(`Category: ${item.querySelector("h2").textContent}`);
-    console.log(`Elements: ${item.querySelector("ul").children.length}`)
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
 });
